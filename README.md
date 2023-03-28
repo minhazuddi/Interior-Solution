f6a0942bcf16e9d261a3f2fe467a3015c5a8c5e4
unknown <minhazuddin4615@gmail.com>
2023-03-28 11:12:11 +0600

    project upload

---
+++ README.md
@@ -0,0 +1,51 @@
+
+# BOOKISH
+
+Bookish is a book store that sells books online. We have a wide range of books from different categories. We also have a blog where we post articles about books and authors.
+
+<img src="https://github.com/mdadul/Bookish/blob/master/img/logo-no-background.png" width="400px">
+
+
+## Author
+
+- [EMDADUL ISLAM](https://github.com/mdadul/)
+
+
+## Run Locally
+
+Please follow the below instructions to run this project in your machine:
+
+1. Clone this repository
+   ```bash
+   git clone https://github.com/mdadul/Bookish.git
+   ```
+2. Open index.html file with VS Code live server plugin.
+
+
+## Features
+
+- Responsive Web Page
+- Add to Cart
+- Cart Product Count
+- Product Price calculation
+
+## Lessons Learned
+
+I learned while building this project JS DOM manipulation & Fetch API
+## 🔗 Links
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://mdadul.github.io/)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/emdadulislam/)
+
+
+## Tech Stack
+
+**Client:** HTML, CSS, Js, DOM, API
+
+
+## 🛠 Skills
+Javascript, HTML, CSS...
+
+
+## Feedback
+
+If you have any feedback, please reach out to us at emdadulislam162@gmail.com
---
+++ script.js
@@ -0,0 +1,80 @@
+let count = 0;
+const countWallpaper = (price) => {
+  count++;
+  const cart = (document.getElementById("total-product").innerText = count);
+  updatePrice(price);
+  total();
+};
+const removePaper = (price) => {
+  if (count > 0) {
+    count = count - 1;
+    const cart = (document.getElementById("total-product").innerText = count);
+    const oldPrice = parseFloat(document.getElementById("price").innerText);
+    const newPrice = oldPrice - price;
+    if (newPrice < 0) {
+      newPrice = 0;
+    }
+    document.getElementById("price").innerText = newPrice.toFixed(2);
+    deliveryCharge(newPrice);
+  }
+};
+
+const updatePrice = (price) => {
+  const oldPrice = parseFloat(document.getElementById("price").innerText);
+  const newPrice = oldPrice + price;
+  document.getElementById("price").innerText = newPrice.toFixed(2);
+  deliveryCharge(newPrice);
+};
+const deliveryCharge = (price) => {
+  let charge = 0;
+  if (price < 500) {
+    charge = 0;
+  } else if (price >= 500 && price < 800) {
+    charge = 100;
+  } else if (price >= 800 && price < 1000) {
+    charge = 150;
+  } else {
+    charge = 200;
+  }
+  document.getElementById("delivery-charge").innerText = charge;
+  document.getElementById("shipping-charge").innerText = charge;
+  subtotal = price + charge + charge;
+  document.getElementById("subtotal").innerText = subtotal.toFixed(2);
+  tax(subtotal);
+};
+const tax = (subtotal) => {
+  const tax = subtotal * 0.15;
+  document.getElementById("tax").innerText = tax.toFixed(2);
+  subtotal += tax;
+  total(subtotal);
+};
+const total = (total) => {
+  document.getElementById("total-price").innerText = total.toFixed(2);
+  const discount = total * 0.05;
+  document.getElementById("dis-price").innerText = discount.toFixed(2);
+  const finalPrice = total - discount;
+  document.getElementById("final-price").innerText = finalPrice.toFixed(2);
+};
+
+const greetings = () => {
+  const totalPrice = parseFloat(
+    document.getElementById("total-price").innerText
+  );
+  if (totalPrice == 0) {
+    swal("Please add some books at cart", "", "error");
+  } else {
+    swal(
+      "Thanks for your Order",
+      "Your total price is: " + totalPrice,
+      "success"
+    );
+    count = 0;
+    document.getElementById("total-product").innerText = count;
+    document.getElementById("price").innerText = 0;
+    document.getElementById("delivery-charge").innerText = 0;
+    document.getElementById("shipping-charge").innerText = 0;
+    document.getElementById("subtotal").innerText = 0;
+    document.getElementById("tax").innerText = 0;
+    document.getElementById("total-price").innerText = 0;
+  }
+};
